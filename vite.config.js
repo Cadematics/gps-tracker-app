c430066cdaf49eff3393d23983e3c00b0ebdf3e4
mahomagh <mahomagh@gmail.com>
2025-11-30 06:28:37 +0000

    hopfully vercel deplyment will work

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,25 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react()],
+//     test: {
+//     tailwindcss(),
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: './src/setupTests.js',
+//   },
+// });
+
+
+
+
+
 export default defineConfig({
   plugins: [
-    react()],
-    tailwindcss(),
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-  },
-});
-
-
+    react(),
+    tailwindcss()
+  ]
+})
