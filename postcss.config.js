583f22f0cf576fa7c514f8498ad95f176e928090
mahomagh <mahomagh@gmail.com>
2025-11-30 06:16:48 +0000

    Fix PostCSS Tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,8 @@
-// export default {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-
-
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
+
+
